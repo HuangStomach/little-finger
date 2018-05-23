98e6c7525731cd76e442abd22e3614addb64a6fb
zhangchunxia <zcx1458@163.com>
2018-05-23 13:39:24 +0800

    小球不能叠加

--- src/components/ServerItem.js
+++ src/components/ServerItem.js
@@ -7,6 +7,7 @@ import 'echarts/lib/chart/graph';
 // 引入提示框和标题组件
 import 'echarts/lib/component/tooltip';
 import 'echarts/lib/component/title';
+import { Circle } from 'echarts/lib/util/graphic';
 //列表项
 class ServerItem extends React.Component{
   componentDidMount() {
@@ -15,12 +16,14 @@ class ServerItem extends React.Component{
     const dataNode = [];
     this.props.data.map((item, index) => {
       const opt = {};
-      const x = Math.random() * 800 + 1;
-      const y = Math.random() * 800 + 1;
+      //const x = Math.random() * 600 + 300;
+      const x = Math.random()*(600-300+1)+300;
+      //const y = Math.random() * 600 + 300;
+      const y = Math.random()*(600-300+1)+300;
       opt.name = item.name;
       opt.x = x;
       opt.y = y;
-      opt.symbolSize = item.active == 1 ? '80':'40';
+      opt.symbolSize = item.active == 1 ? '70':'35';
       opt.fqdn = item.fqdn,
       opt.address = item.address,
       opt.update = item.update,
@@ -34,33 +37,38 @@ class ServerItem extends React.Component{
           text: '服务器状态显示'
         },
         tooltip: {
-          formatter: '{b}'
+          formatter: '{b}',
+          trigger: 'item',
+        //   formatter: function (dataNode) {
+        //         for (var i=0;i<dataNode.length;i++){
+        //           return ('name'+dataNode[i].name+
+        //                     '<br/>fqdn'+dataNode[i].fqdn+
+        //                     '<br/>address'+dataNode[i].address+
+        //                     '<br/>update'+dataNode[i].update)
+        //        }
+        //   }
         },
-        // legend: {
-        //     data:['name','fqdn','address','update']
-        // },
+        legend: {
+          data:['name','fqdn','address','update']
+        },
         animationDurationUpdate: 1500,
         animationEasingUpdate: 'quinticInOut',
         series : [
           {
             type: 'graph',
-            layout: 'none',
-            symbolSize: 90,
+            layout: 'force',
+            // symbolSize: 90,
             roam: false,
             label: {
-                normal: {
-                    show: true
-                }
+              normal: {
+                show: true
+              }
             },        
-            type: 'graph',
-            layout: 'none',
-            symbolSize: 90,
-            roam: false,
-            label: {
-                normal: {
-                    show: true
-                }
-            },       
+            force: {
+              repulsion: 100,
+              edgeLength: 80,
+              gravity:1
+            },    
             data: dataNode, 
           }
         ]                    
