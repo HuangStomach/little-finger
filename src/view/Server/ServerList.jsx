64d9f5576ec301a7979364bcb060fcc4ab136f6d
wanghaiqing <2281980665@qq.com>
2018-07-27 17:51:05 +0800

    获取服务器列表数据

--- src/view/Server/ServerList.jsx
+++ src/view/Server/ServerList.jsx
@@ -1,36 +1,38 @@
 import React, { Component } from 'react';
 import { autorun, computed } from 'mobx';
 import { inject, observer } from 'mobx-react';
-import Style from './List.css';
 import { Table, Button } from 'antd';
 
-
-const columns = [
-  {title: '名称', dataIndex: 'name',width:'10vw'},
-  {title: 'LAB', dataIndex: 'lab',width:'10vw'},
-  {title: 'SITE', dataIndex: 'site',width:'10vw'},
-  {title: 'FQDN', dataIndex: 'fqdn',width:'20vw'},
-  {title: '地址', dataIndex: 'address',width:'20vw'},
-  {title: '路径', dataIndex: 'path',width:'30vw'},
-];
-
-const data = [];
-for (let i = 0; i < 46; i++) {
-  data.push({
-    key: i,
-    name:'name',
-    lab:'lab',
-    site:'site',
-    fqdn: `Edward King ${i}`,
-    address: `London, Park Lane no. ${i}`,
-    path: `London, Park Lane no. ${i}`,
-  });
-}
-
-
-
+@inject('ServerStore')
+@observer
 class ServerList extends Component{
+  @computed get servers() {
+    return this.props.ServerStore.servers;
+  }
+
   render() {
+    const columns = [
+      {title: '名称', dataIndex: 'name',width:'10vw'},
+      {title: 'LAB', dataIndex: 'lab',width:'10vw'},
+      {title: 'SITE', dataIndex: 'site',width:'10vw'},
+      {title: 'FQDN', dataIndex: 'fqdn',width:'20vw'},
+      {title: '地址', dataIndex: 'address',width:'20vw'},
+      {title: '路径', dataIndex: 'path',width:'30vw'},
+    ];
+
+    const data = [];
+    for (let i = 0; i < this.servers.length; i++) {
+      data.push({
+        key: i,
+        name:this.servers[i].name,
+        lab:this.servers[i].lab,
+        site:this.servers[i].site,
+        fqdn:this.servers[i].fqdn,
+        address:this.servers[i].address,
+        path:this.servers[i].path,
+      });
+    }
+
     return (
       <Table  columns={columns} dataSource={data} pagination={{ pageSize: 20 }} scroll={{ y: '60vh' }} size="middle"/>
     );
